2c2b8e0062aa1c2c77cc932a0b048515f77ab630
Sergei Shatunov <sshatunov@gmail.com>
2020-09-06 15:32:18 -0400

    Slice

---
+++ github-actions-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github-actions-test
+//
+//  Created by Sergei Shatunov on 9/6/20.
+//  Copyright © 2020 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
